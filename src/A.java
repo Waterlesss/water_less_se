43d533f4dc72c13837b804ce424bbb9ac7cb0f4d
职回水 <1554333496@qq.com>
2022-04-29 23:33:39 +0800

    abstract关键字的test

---
+++ src/A.java
@@ -0,0 +1,16 @@
+public abstract class A {
+    public abstract void fun();
+
+    public static void main(String[] args) {
+        A a = new C();
+        a.fun();
+    }
+}
+
+ class C extends A {
+
+     @Override
+     public void fun() {
+         System.out.println("C覆写A的抽象方法");
+     }
+ }
