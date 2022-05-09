5f1595c56c01ab287ea2b6778b4e9326c26cc564
职回水 <1554333496@qq.com>
2022-05-09 16:12:26 +0800

    LeetCode NO.290 problem

--- src/string_test/StringTest.java
+++ src/string_test/StringTest.java
@@ -1,29 +1,98 @@
 package string_test;
 
+import java.sql.SQLOutput;
+
 public class StringTest {
+//    String pattern = "abba";
+//    String s;
+    public boolean wordPattern(String pattern, String s) {
+
+        return false;
+    }
     public static void main(String[] args) {
+        String s;
+        String pattern = "abba";
+        char[] data = pattern.toCharArray();
+        int i = 0;
+        System.out.println(data[0]==);
 //        String str ="hello world";
 //        String str1 = str;
 //        str1 = "Hello";
 //        System.out.println(str);
+
+        //比较字符串的相等
 //        String str1 = "hello";
 //        String str2 = "hello";
 //        String str1 = new String("hello");
-//        String str2 = new String("hello");
-//        System.out.println(str1.equals(str2));
+//        String str2 = new String("Hello");
+//        System.out.println(str1.equalsIgnoreCase(str2));
 //        System.out.println(str1==str2);
+
 //        String userName = null;
 //        System.out.println(userName.equals("张三"));
 //        System.out.println("张三".equals(userName));
+
+//        String str1 = "abc";
+//        String str2 = "Abc";
+//        System.out.println(str1.compareTo(str2));
+
 //        String str1 = "hello";
 //        String str2 = "hello";
 //        String str3 = "hello";
-        String str1 = new String("hello");
-        String str2 = new String("hello");
-        String str3 = new String("hello");
-        System.out.println(str1==str2);
-        System.out.println(str2==str3);
-    }
 
+//        String str1 = new String("hello");
+//        String str2 = new String("hello");
+//        String str3 = new String("hello");
+//        System.out.println(str1==str2);
+//        System.out.println(str2==str3);
+
+        //这个str1指向堆中普通的字符串对象
+//        String str1 = new String("hello");
+//        str1 = str1.intern();
+//        String str2 = "hello";
+//        System.out.println(str1 == str2);
+            //intern方法手工入池
+//        char[] data = new char[] {'a','b','c'};
+//        String str1 = new String(data);
+//        str1.intern();
+//        String str2 = "abc";
+//        System.out.println(str1 == str2);
+
+//        char[] ch = new char[] {'a','b','c'};
+//        String str = new String(ch);
+//        String str1 = new String(ch,1,2);
+//        String str1 = String.valueOf(ch);
+//        System.out.println(str);
+//        System.out.println(str1);
+//        String str = "hello";
+//        char[] data = str.toCharArray();
+//        System.out.println(data);
+//        System.out.println(str.charAt(1));
+
+//        String str = "hello";
+//        char[] data = str.toCharArray();
+//        System.out.println(data);
+//        System.out.println(data[0]);
+
+//        String str1 = "123";
+//        String str2 = "123a";
+//        System.out.println(isNumber(str1));
+//        System.out.println(isNumber(str2));
+        }
+      //传入一个string对象，判断是否由纯数字组成
+//    public static boolean isNumber(String str) {
+//        //转为字符数组
+//        char[] data = str.toCharArray();
+//        //循环遍历data中的每个字符，判断这个字符是否是数字字符
+//        for (char c : data) {
+////            if (c < '0' || c > '9') {
+////                return false;
+////            }
+//            if (!Character.isDigit(c)) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
 
 }
