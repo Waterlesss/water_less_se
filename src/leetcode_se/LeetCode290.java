48d57d89e31e582c645ccc60432db0391654871b
职回水 <1554333496@qq.com>
2022-05-09 16:12:57 +0800

    LeetCode NO.290 problem

---
+++ src/leetcode_se/LeetCode290.java
@@ -0,0 +1,36 @@
+package leetcode_se;
+
+public class LeetCode290 {
+    class Solution {
+        public boolean wordPattern(String pattern, String s) {
+            char[] data1 = pattern.toCharArray();
+            String[] str = s.split(" ");
+            if(data1.length==1 && str.length==1){
+                return true;
+            }
+            if (data1.length == str.length) {
+
+                for (int j = 0;j<data1.length-1;j++){
+                    int i = j;
+                    while (i < data1.length -1) {
+                        if (data1[j]==data1[i+1]) {
+                            if (str[j].equals(str[i+1])) {
+                                i++;
+                            } else {
+                                return false;
+                            }
+                        } else {
+                            if (!str[j].equals(str[i+1])) {
+                                i++;
+                            } else{
+                                return false;
+                            }
+                        }
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
