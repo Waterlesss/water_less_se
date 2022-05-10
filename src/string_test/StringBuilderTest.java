132b71f7e2ba00fd3d3a89ca5f49135ea51ba0b3
职回水 <1554333496@qq.com>
2022-05-10 11:37:07 +0800

    StringBuilder和String类的互相转换及StringBuilder的其他操作

--- src/string_test/StringBuilderTest.java
+++ src/string_test/StringBuilderTest.java
@@ -7,11 +7,19 @@ public class StringBuilderTest {
 //        sb.append("world");
 //        sb.append("!");
 //        System.out.println(sb);
+        /**
+         * String和StringBuilder类不能直接转换
+         * 转为StringBuilder：利用StringBuilder的构造方法或append方法
+         * 转为String：调用String类的toString方法
+         */
         StringBuilder sb = new StringBuilder("hello");
 //        sb.append("123");
         sb.append("world");
+        //删除指定范围的数据
         sb.delete(5,10);
+        //字符串反转
 //        sb.reverse();
+        //插入数据
         sb.insert(5,10);
         String str = sb.toString();
         System.out.println(str);
