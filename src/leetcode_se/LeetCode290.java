6831f0dceb16f0551fa8a21ffa413b85f9e4bb63
职回水 <1554333496@qq.com>
2022-05-11 15:26:10 +0800

    LeetCode290 problem do not use Hash Map

--- src/leetcode_se/LeetCode290.java
+++ src/leetcode_se/LeetCode290.java
@@ -5,24 +5,24 @@ public class LeetCode290 {
         public boolean wordPattern(String pattern, String s) {
             char[] data1 = pattern.toCharArray();
             String[] str = s.split(" ");
-            if(data1.length==1 && str.length==1){
+            if (data1.length == 1 && str.length == 1) {
                 return true;
             }
             if (data1.length == str.length) {
 
-                for (int j = 0;j<data1.length-1;j++){
+                for (int j = 0; j < data1.length - 1; j++) {
                     int i = j;
-                    while (i < data1.length -1) {
-                        if (data1[j]==data1[i+1]) {
-                            if (str[j].equals(str[i+1])) {
+                    while (i < data1.length - 1) {
+                        if (data1[j] == data1[i + 1]) {
+                            if (str[j].equals(str[i + 1])) {
                                 i++;
                             } else {
                                 return false;
                             }
                         } else {
-                            if (!str[j].equals(str[i+1])) {
+                            if (!str[j].equals(str[i + 1])) {
                                 i++;
-                            } else{
+                            } else {
                                 return false;
                             }
                         }
